afb7ff1bde2245ccc448823900bf4f393902fdfd
Jacob Foshee <jacobf@gmail.com>
2015-03-05 11:16:17 -0600

    Can write 3D array of doubles. Yields a file: >h5dump .\sample.h5 HDF5 ".\sample.h5" { GROUP "/" {    DATASET "DS1" {       DATATYPE  H5T_IEEE_F64LE       DATASPACE  SIMPLE { ( 2, 3, 4 ) / ( 2, 3, 4 ) }       DATA {       (0,0,0): 2, 4, 8, 16,       (0,1,0): 3, 9, 27, 81,       (0,2,0): 4, 16, 64, 256,       (1,0,0): 1, 2, 4, 8,       (1,1,0): 1.5, 4.5, 13.5, 40.5,       (1,2,0): 2, 8, 32, 128       }    } } }

--- README.md
+++ README.md
@@ -4,5 +4,6 @@ Learning [HDF5DotNet](http://hdf5.net)
 An exercise in learning the API for HDF 5 I/O using C# and the managed interface
 
 - [Reading a matrix of doubles](TryHDF/Reading.cs)
+- [Writing a 3D array of doubles](TryHDF/Writing.cs)
 
 The sample data and binaries are from the [HDF Group](http://www.hdfgroup.org/)
---
+++ TryHDF/Writing.cs
@@ -0,0 +1,37 @@
+﻿using HDF5DotNet;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace TryHDF
+{
+    [TestClass]
+    public class Writing
+    {
+        [TestMethod]
+        public void CanWriteDoubles()
+        {
+            var f = H5F.create("sample.h5", H5F.CreateMode.ACC_TRUNC);
+            {
+                var dimensions = new long[] {2, 3, 4};
+                var s = H5S.create_simple(rank: dimensions.Length, dims: dimensions);
+                {
+                    var type = H5T.copy(H5T.H5Type.NATIVE_DOUBLE);
+                    var d = H5D.create(f, "DS1", type, s);
+                    {
+                        var buffer = new double[2, 3, 4];
+                        for (int i = 0; i < 2; i++)
+                            for (int j = 0; j < 3; j++)
+                                for (int k = 0; k < 4; k++)
+                                    buffer[i, j, k] = Math.Pow(j + 2, k + 1) / (i + 1);
+                        H5D.write<double>(d, type, new H5Array<double>(buffer));
+                    }
+                    H5D.close(d);
+                }
+                H5S.close(s);
+            }
+            H5F.close(f);
+            Assert.IsTrue(File.Exists("sample.h5"));
+        }
+    }
+}
